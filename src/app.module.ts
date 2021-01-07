8d5751d32ba185c6a6809b1aee2859a05f3e7012
root <root@DESKTOP-H022FR1.localdomain>
2021-01-07 20:02:39 +0900

    #2.4 Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,30 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: process.env.NODE_ENV == "dev" ? ".env.dev" : "env.test"
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "NB",
+      password: "password",
+      database: "nuber-eats",
+      synchronize: true,
+      logging: true,
+    }),
+    RestaurantsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: true,
-    }),
-    RestaurantsModule
+    })
   ],
   controllers: [],
   providers: [],
