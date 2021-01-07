316304ec8550c68be8e6b72f816224f900daeead
root <root@DESKTOP-H022FR1.localdomain>
2021-01-07 20:23:21 +0900

    #2.5 Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,15 +10,16 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
-      envFilePath: process.env.NODE_ENV == "dev" ? ".env.dev" : "env.test"
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
     }),
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "NB",
-      password: "password",
-      database: "nuber-eats",
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
