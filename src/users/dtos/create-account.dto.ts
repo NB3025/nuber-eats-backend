0e9ff0d9e801ca11468333bc4e0a316edf1f0fc2
NB <over3025@gmail.com>
2021-01-13 23:39:45 +0900

    5.13 updateProfile part One

--- src/users/dtos/create-account.dto.ts
+++ src/users/dtos/create-account.dto.ts
@@ -1,11 +1,11 @@
 import { Field, InputType, ObjectType, PickType } from "@nestjs/graphql";
-import { MutationOutput } from "src/common/dtos/output.dto";
+import { CoreOutput } from "src/common/dtos/output.dto";
 import { User } from "../eitities/user.entity";
 
 @InputType()
 export class CreateAccountInput extends PickType(User, ["email", "password", "role"]) { }
 
 @ObjectType()
-export class CreateAccountOutput extends MutationOutput {
+export class CreateAccountOutput extends CoreOutput {
 
 }
